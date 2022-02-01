a544a1fbfb0ed004ddd17e5eb40d2a0aab523ae6
Etereke <98815314+Etereke@users.noreply.github.com>
2022-02-01 16:49:11 +0100

    Update Program.cs
    New version, after code review

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -8,50 +8,33 @@ namespace GuessTheNumber
 {
     public class GuessingGame
     {
-        private static int readNumber()
+        private static Random rnd = new Random();
+        private const int UPPER_BOUND = 1000000000;
+        private int ReadNumber()
         {
-            bool isNumber = false;
-            int number = 0;
-            do
+            int number;
+            string numberString = Console.ReadLine();
+            while (!int.TryParse(numberString, out number))
             {
-                string numberString = Console.ReadLine();
-                try
-                {
-                    number = int.Parse(numberString);
-                    isNumber = true;
-                }
-                catch
-                {
-                    Console.WriteLine("\"{0}\" is not a valid number!", numberString);
-                }
-            } while (!isNumber);
-
+                Console.WriteLine("\"{0}\" is not a valid number!", numberString);
+                numberString = Console.ReadLine();
+            }
             return number;
         }
 
-        private static int setRange()
+        private int SetRange()
         {
-            Console.WriteLine("Enter the range (1 to chosen number, maximum 1 billion): ");
-            int range = readNumber();
-
-            bool correctRange = false;
-            do
+            Console.WriteLine("Enter the range (1 to chosen number, minimum 2, maximum 1 billion): ");
+            int range = ReadNumber();
+            while (range <= 1 || range > UPPER_BOUND)
             {
-                if (range > 1 && range <= 1000000000)
-                {
-                    correctRange = true;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid range!");
-                    range = readNumber();
-                }
-            } while (!correctRange);
-
+                Console.WriteLine("Invalid range!");
+                range = ReadNumber();
+            }
             return range;
         }
 
-        private static int calculateNumberOfGuesses(int range)
+        private int CalculateNumberOfGuesses(int range)
         {
             int numberOfGuesses = 0;
             while (range != 0)
@@ -61,58 +44,71 @@ namespace GuessTheNumber
             }
             return numberOfGuesses;
         }
-
-        private static int thinkOfANumber(int range)
+        private void WriteTip(int guess, int number)
         {
-            Random rnd = new Random();
-            return rnd.Next(1, range);
-        }
-
-        
-        private static bool game(int range)
-        {
-            int numberOfGuesses = calculateNumberOfGuesses(range);
-            int number = thinkOfANumber(range);
-
-            bool isWon = false;
-            do
+            if (guess > number)
             {
-                Console.WriteLine("You have {0} guesses left!\n" +
-                                    "Your guess: ", numberOfGuesses);
-                int guess = readNumber();
-
-                if (guess == number) isWon = true;
-                else
-                {
-                    if (guess > number)
-                    {
-                        Console.WriteLine("Try something lower!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Try something higher!");
-                    }
-                    numberOfGuesses--;
-                }
-            } while (numberOfGuesses != 0 && isWon != true);
-            return isWon;
-        }
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Guess the number game");
-            int range = setRange();
-
-            bool isWon = game(range);
-            if (isWon)
-            {
-                Console.WriteLine("Congratulations! You guessed correctly! You win!");
+                Console.WriteLine("Try something lower!");
             }
             else
             {
-                Console.WriteLine("You are out of guesses! You lose!");
+                Console.WriteLine("Try something higher!");
             }
-
-            Console.ReadLine();
+        }
+        private bool SingleRound(int range)
+        {
+            Console.WriteLine("\nNew game started");
+            int numberOfGuesses = CalculateNumberOfGuesses(range);
+            int number = rnd.Next(1, range);
+            do
+            {
+                Console.WriteLine("\nYou have {0} guesses left!\n" +
+                                    "Your guess: ", numberOfGuesses);
+                int guess = ReadNumber();
+                numberOfGuesses--;
+                if (guess == number)
+                {
+                    Console.WriteLine("Congratulations! You guessed correctly! You win!");
+                    return true;
+                }
+                else if (numberOfGuesses > 0)
+                {
+                    WriteTip(guess, number);
+                }
+            } while (numberOfGuesses > 0);
+            Console.WriteLine("You are out of guesses! You lose!");
+            return false;
+        }
+        public void PlayGame()
+        {
+            Console.WriteLine("Guess the number game");
+            int command;
+            do
+            {
+                Console.WriteLine("\nType [1] to start a new game or [0] to quit");
+                command = ReadNumber();
+                switch (command)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        SingleRound(SetRange());
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command!");
+                        break;
+                }
+            } while (command != 0);
+        }
+    }
+        
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            GuessingGame guessingGame = new GuessingGame();
+            guessingGame.PlayGame();
         }
     }
 }
+
